8f4deae52f4f624dc3e0f9c5e7cd2e5a6aa1acdf
Andrew Moyes <moyes.andrew@gmail.com>
2023-02-03 09:20:07 +0100

    working on ast

--- compiler/AmlAst.fs
+++ compiler/AmlAst.fs
@@ -30,33 +30,17 @@ module AST =
 
     type Function = Identifier * FunctionOperator * Expression
 
-    and CalledFunction =
-        | NamedFunctionCall of Identifier * Identifier
-        | AnonymousFunctionCall of GroupOpen * Function * GroupClose * Identifier
-
-    and SubExpression =
-        | Identifier of Identifier
-        | Primitive of Primitive
-        | Function of Function
-
     and Expression =
-        | SimpleEnclosedExpression of GroupOpen * SubExpression * GroupClose
-        | NestedEnclosedExpression of GroupOpen * Expression * GroupClose
+        | AnonymousFunctionCall of GroupOpen * Function * GroupClose * Expression
+        | Boolean of Boolean
+        | Function of Function
+        | Identifier of Identifier
         | LineExpression of Expression * EndOfLine
-        | CalledFunction of CalledFunction
-
-    and Statement =
-        | LetDeclaration of LetDeclaration
-        | CalledFunction of CalledFunction
+        | NamedFunctionCall of Identifier * Expression
+        | NestedEnclosedExpression of GroupOpen * Expression * GroupClose
+        | Number of Number
+        | String of String
+        | Structure of StructOpen * (Identifier * StructEntryNameValueSeperator * Expression * FieldSeparator) list option * (SplatOperator * FieldSeparator) option * StructClose
 
     and LetDeclaration =
         VariableDeclaration * Identifier * (TypeBind * InfixKeyword option * TypeSignature) option * SymbolBind * Expression
-
-    and Primitive =
-        | Number of Number
-        | String of String
-        | Boolean of Boolean
-        | Structure of Structure
-
-    and Structure =
-        StructOpen * (Identifier * StructEntryNameValueSeperator * Expression * FieldSeparator) list option * (SplatOperator * FieldSeparator) option * StructClose
